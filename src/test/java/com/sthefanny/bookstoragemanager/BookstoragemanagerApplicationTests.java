7b7effcaa7ca6f97a8e4c550c4f73b99837243ea
larissasthefanny <sthefyguedess@gmail.com>
2021-12-29 10:01:10 -0300

    first commit

---
+++ src/test/java/com/sthefanny/bookstoragemanager/BookstoragemanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sthefanny.bookstoragemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoragemanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
